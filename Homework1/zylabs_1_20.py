d9ad8762dd158269ac5e720325e076299e5aa57f
jprebele <99056092+jprebele@users.noreply.github.com>
2023-06-16 11:15:25 -0500

    Adding zylabs and problem 1

---
+++ Homework1/zylabs_1_20.py
@@ -0,0 +1,12 @@
+# John Rebeles
+# PSID:2039426
+
+print("Enter integer:")
+user_num = int(input())
+print("You entered:", user_num)
+print(user_num, "squared is",  user_num * user_num)
+print("And", user_num, "cubed is",  user_num**3, '!!')
+print("Enter another integer:")
+user_num2 = int(input())
+print(user_num, '+',  user_num2, 'is', user_num + user_num2)
+print(user_num, '*',  user_num2, 'is', user_num * user_num2)
