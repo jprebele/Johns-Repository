ccddb8389861f1bb30439707d98688e6d6e220ab
jprebele <99056092+jprebele@users.noreply.github.com>
2023-06-16 11:12:17 -0500

    Delete wrong file

--- Homework1/zylabs_1_12.py
+++
@@ -1,8 +0,0 @@
-# John Rebeles
-# PSID:22039426
-
-userNum = int(input())
-userNumSquared = userNum * userNum  # Bug here; fix it when instructed
-
-print(userNumSquared)
-# Output formatting issue here; fix it when instructed
