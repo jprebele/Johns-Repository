7eaf94c9bbd920bc8a116a2742e4f516471ab96c
jprebele <99056092+jprebele@users.noreply.github.com>
2023-07-01 22:38:13 -0500

    Adding zylab 9.10

---
+++ homework2/zylab_9_10.py
@@ -0,0 +1,17 @@
+import csv
+
+input_file = input()
+with open(input_file, 'r') as csvfile:
+    words = csv.reader(csvfile, delimiter=',')
+
+    number_of_words = {}
+
+    for i in words:
+        for j in i:
+            if j not in number_of_words:
+                number_of_words[j] = 1
+            else:
+                number_of_words[j] += 1
+
+    for j, num in number_of_words.items():
+        print(f'{j} {num}')
